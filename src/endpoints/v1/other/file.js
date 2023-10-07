d729a2823429411f8d8e89b33641171693de63a6
Kath <55346310+Kathund@users.noreply.github.com>
2023-10-07 14:54:56 +0800

    Logging

--- src/endpoints/v1/other/file.js
+++ src/endpoints/v1/other/file.js
@@ -10,12 +10,17 @@ module.exports = (app) => {
     const id = req.query.id;
     if (!id) return res.status(400).end('You are missing an id');
     var userData = JSON.parse(fs.readFileSync('userData.json', 'utf8'));
-    if (!userData[id]) return res.status(400).end('You do not have access to this file');
+    if (!userData[id]) {
+      errorMessage(`file ${id} was not found - Requested by ${req.headers['x-forwarded-for']}`);
+      return res.status(400).end('You do not have access to this file');
+    }
     if (userData[id].tickets.includes(requestedFileName) || userData[id].admin) {
       const filePath = path.join(path.join(__dirname, config.ticketFolder), `${requestedFileName}.txt`);
       fs.stat(filePath, (err, stats) => {
         if (err || !stats.isFile() || path.extname(filePath) !== '.txt') {
-          errorMessage(`File ${filePath} was requested by ${req.headers['x-forwarded-for']} but was not found`);
+          errorMessage(
+            `File ${filePath} was requested by ${req.headers['x-forwarded-for']} but was not found with ${id}`
+          );
           return res.status(404).end('File not found');
         }
 
@@ -24,7 +29,10 @@ module.exports = (app) => {
             errorMessage(`Error reading file: ${err}`);
             return res.status(500).end('Internal Server Error');
           }
-          apiMessage('/:file.txt', `File ${filePath} was requested by ${req.headers['x-forwarded-for']} and sent`);
+          apiMessage(
+            '/:file.txt',
+            `File ${filePath} was requested by ${req.headers['x-forwarded-for']} and sent with ${id}`
+          );
           return res.set('Cache-Control', 'public, max-age=600').status(200).end(data);
         });
       });
